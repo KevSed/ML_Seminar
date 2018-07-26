79f1e9c2ff1a18657a703504190a532a8ed43f87
Kevin Sedlaczek <kevin.sedlaczek@tu-dortmund.de>
2018-07-26 14:32:05 +0200

    function for history plotting

--- plot_history.py
+++ plot_history.py
@@ -1,20 +1,32 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_history(network_history):
+
+def plot_history(loss, val_loss, acc, val_acc, name):
+
+    a = 0.7
+    b = (1 - a)
+    a *=  10**4
+    b *=  10**4
+    if name in 'equal':
+        a = 1
+        b = 1
     plt.figure()
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
-    plt.plot(network_history.history['loss'])
-    plt.plot(network_history.history['val_loss'])
+    plt.plot(loss*(np.ones(len(loss))*a))
+    plt.plot(val_loss*(np.ones(len(loss))*b))
+    plt.title('Loss history')
     plt.legend(['Training', 'Validation'])
-    plt.savefig('loss_history.pdf')
+    plt.savefig('pdf/loss_history_{}.pdf'.format(name))
     plt.close()
 
     plt.figure()
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
-    plt.plot(network_history.history['acc'])
-    plt.plot(network_history.history['val_acc'])
+    plt.plot(acc)
+    plt.plot(val_acc)
+    plt.title('Accuracy history')
     plt.legend(['Training', 'Validation'], loc='lower right')
-    plt.savefig('accuracy_history.pdf')
+    plt.savefig('pdf/accuracy_history_{}.pdf'.format(name))
     plt.close()
